9d945496e3ddd05d90eb6ef5199af812c68905b4
Ayush Chaudhary <ayush0000ayush@gmail.com>
2024-10-27 00:05:07 +0530

    better website init

--- frontend/src/const.ts
+++
@@ -1,5 +0,0 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const SUPABASE = {
-  PROJECT_URL: import.meta.env.VITE_SUPABASE_PROJECT_URL as string,
-  ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY as string,
-};
